542c0ddeafdaafb49b925f89fe2aa64425e752e4
Frozenlock <frozenlock@gmail.com>
2012-11-28 15:28:19 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject bacnet-scan-utils "1.0.8"
+(defproject bacnet-scan-utils "1.0.9"
   :description "Small library to scan a BACnet network."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.jarpiain/bacnet4j "1.2.0"]
-                 [org.clojars.jarpiain/sero-utils "1.0.0"]
+                 [org.clojars.frozenlock/bacnet4j "1.2.4"]
                  [clj-time "0.4.2"]
                  [hiccup "1.0.0-beta1"]
                  [org.clojars.frozenlock/gzip64 "1.0.0"]])
